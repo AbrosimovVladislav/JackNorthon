b62375073cc523ffdc62fb1e875c79411bcca4a2
AbrosimovVladislav <abrosimov94vladislav@gmail.com>
2020-03-26 00:41:24 +0300

    feature/product: car and carservice and carView added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule} from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {DataViewModule} from 'primeng/dataview';
 import {ButtonModule} from 'primeng/button';
-import { ProductPreviewComponent } from './components/product-preview/product-preview.component';
-import { ProductCardComponent } from './components/product-card/product-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductPreviewComponent,
-    ProductCardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -23,4 +19,5 @@ import { ProductCardComponent } from './components/product-card/product-card.com
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
